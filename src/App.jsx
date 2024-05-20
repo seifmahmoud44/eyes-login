d0aab5467167be55dc66338a5fd9c5c41afb2173
seif <seenaseif@gmail.com>
2024-05-21 01:36:33 +0300

    finished map component

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,17 @@
-import { useEffect, useRef, useState } from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import iconUrl from "./assets/pin.png"; // Import your custom pin image
+import { useState } from "react";
+import Map from "./components/Map";
 
-const MapComponent = () => {
-  const mapRef = useRef(null);
-  const mapInstanceRef = useRef(null);
-  const markerRef = useRef(null);
-  const [position, setPosition] = useState({ lat: 51.505, lng: -0.09 }); // Default position
-
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setPosition({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        (error) => {
-          console.error("Error getting location: ", error);
-        },
-        {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0,
-        }
-      );
-    }
-  }, []);
-
-  useEffect(() => {
-    if (mapInstanceRef.current) return;
-
-    mapInstanceRef.current = L.map(mapRef.current).setView(
-      [position.lat, position.lng],
-
-      15
-    );
-
-    // Satellite tile layer
-    const satelliteLayer = L.tileLayer(
-      "https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-      {
-        maxZoom: 15,
-        subdomains: ["mt0", "mt1", "mt2", "mt3"],
-        attribution:
-          'Map data &copy; <a href="https://www.google.com/maps">Google Maps</a>',
-      }
-    );
-
-    // Street tile layer (default)
-    const streetLayer = L.tileLayer(
-      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }
-    );
-
-    // Add default layer to the map
-    streetLayer.addTo(mapInstanceRef.current);
-
-    // Add layer control to switch between satellite and street layers
-    L.control
-      .layers({
-        "Street View": streetLayer,
-        "Satellite View": satelliteLayer,
-      })
-      .addTo(mapInstanceRef.current);
-
-    // Custom icon for marker
-    const customIcon = L.icon({
-      iconUrl: iconUrl, // Path to your custom pin image
-      iconSize: [32, 32], // Size of the image
-      iconAnchor: [16, 32], // Position of the image relative to the marker's location
-    });
-
-    markerRef.current = L.marker([position.lat, position.lng], {
-      icon: customIcon,
-    }).addTo(mapInstanceRef.current);
-
-    mapInstanceRef.current.on("click", function (e) {
-      setPosition({ lat: e.latlng.lat, lng: e.latlng.lng });
-    });
-
-    return () => {
-      mapInstanceRef.current.remove();
-      mapInstanceRef.current = null;
-    };
-  }, []);
-  useEffect(() => {
-    if (mapInstanceRef.current && markerRef.current) {
-      mapInstanceRef.current.setView([position.lat, position.lng], 15);
-      markerRef.current.setLatLng([position.lat, position.lng]);
-    }
-  }, [position]);
-  console.log(position);
+const App = () => {
+  const [location, setLocation] = useState({
+    lat: 31.00015556438264,
+    lng: 29.74384188652039,
+  });
+  console.log(location);
   return (
-    <div id="map" ref={mapRef} style={{ height: "500px", width: "100%" }}></div>
+    <div>
+      <Map location={location} setLocation={setLocation} />
+    </div>
   );
 };
 
-export default MapComponent;
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useRef, useState } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import iconUrl from "../assets/pin.png"; // Import your custom pin image
+
+// eslint-disable-next-line react/prop-types
+const MapComponent = ({ setLocation, location }) => {
+  const mapRef = useRef(null);
+  const mapInstanceRef = useRef(null);
+  const markerRef = useRef(null);
+  const [position, setPosition] = useState(location); // Default position
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setPosition({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error("Error getting location: ", error);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0,
+        }
+      );
+    }
+  }, []);
+
+  useEffect(() => {
+    if (mapInstanceRef.current) return;
+
+    mapInstanceRef.current = L.map(mapRef.current).setView(
+      [position.lat, position.lng],
+      17
+    );
+
+    // Satellite tile layer
+    const satelliteLayer = L.tileLayer(
+      "https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+      {
+        maxZoom: 20,
+        subdomains: ["mt0", "mt1", "mt2", "mt3"],
+        attribution:
+          'Map data &copy; <a href="https://www.google.com/maps">Google Maps</a>',
+      }
+    );
+
+    // Add the satellite layer to the map
+    satelliteLayer.addTo(mapInstanceRef.current);
+
+    // Custom icon for marker
+    const customIcon = L.icon({
+      iconUrl: iconUrl, // Path to your custom pin image
+      iconSize: [32, 32], // Size of the image
+      iconAnchor: [16, 32], // Position of the image relative to the marker's location
+    });
+
+    markerRef.current = L.marker([position.lat, position.lng], {
+      icon: customIcon,
+    }).addTo(mapInstanceRef.current);
+
+    mapInstanceRef.current.on("click", function (e) {
+      setPosition({ lat: e.latlng.lat, lng: e.latlng.lng });
+      setLocation({ lat: e.latlng.lat, lng: e.latlng.lng });
+    });
+
+    return () => {
+      mapInstanceRef.current.remove();
+      mapInstanceRef.current = null;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (mapInstanceRef.current && markerRef.current) {
+      mapInstanceRef.current.setView([position.lat, position.lng], 17);
+      markerRef.current.setLatLng([position.lat, position.lng]);
+    }
+  }, [position]);
+  //   console.log(position);
+  return (
+    <div id="map" ref={mapRef} style={{ height: "100vh", width: "100%" }}></div>
+  );
+};
+
+export default MapComponent;
